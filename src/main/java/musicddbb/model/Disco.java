486485e92f0d5f10073a48570a43049a1b0d1d15
matad <matad@DESKTOP-3HHADIB>
2020-12-05 22:03:16 +0100

    Disco y cancion

--- src/main/java/musicddbb/model/Disco.java
+++ src/main/java/musicddbb/model/Disco.java
@@ -148,4 +148,32 @@ public class Disco {
         cadena+="\n---------------------------------";
         return cadena;
     }
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + id;
+		return result;
+	}
+
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Disco other = (Disco) obj;
+		if (id != other.id)
+			return false;
+		return true;
+	}
+
+
+
+    
+    
 }
