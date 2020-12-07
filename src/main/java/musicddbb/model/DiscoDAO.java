1af1911d95c9562fbf6d591f9d0abf85bedc08fb
Eduardo Carmona <educar200@gmail.com>
2020-12-07 23:29:29 +0100

    Faltan dos Funciones(La relacion y un listar).

--- src/main/java/musicddbb/model/DiscoDAO.java
+++ src/main/java/musicddbb/model/DiscoDAO.java
@@ -1,6 +1,7 @@
 package musicddbb.model;
 
 import java.sql.Date;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -10,92 +11,93 @@ import musicddbb.utils.Connection;
 public class DiscoDAO extends Disco {
 	private boolean persist;
 	private static EntityManager manager;
-	
+
 	private DiscoDAO(int id) {
 		super(id);
-		persist=false;
+		persist = false;
 	}
-	
+
 	private DiscoDAO() {
 		super();
-		persist=false;		
+		persist = false;
 	}
-	
-	private DiscoDAO(String nombre,String foto,Artista creador, Date fecha_produccion,List<Cancion> canciones ) {
-		super(nombre,foto,creador,fecha_produccion,canciones);
-		persist=false;		
+
+	private DiscoDAO(String nombre, String foto, Artista creador, Date fecha_produccion, List<Cancion> canciones) {
+		super(nombre, foto, creador, fecha_produccion, canciones);
+		persist = false;
 	}
-	
-	private DiscoDAO(int id,String nombre,String foto,Artista creador, Date fecha_produccion,List<Cancion> canciones ) {
-		super(id,nombre,foto,creador,fecha_produccion,canciones);
-		persist=false;
-	
+
+	private DiscoDAO(int id, String nombre, String foto, Artista creador, Date fecha_produccion,
+			List<Cancion> canciones) {
+		super(id, nombre, foto, creador, fecha_produccion, canciones);
+		persist = false;
+
 	}
-	
+
 	public void persist() {
-		persist=true;
+		persist = true;
 	}
-	
+
 	public DiscoDAO(Disco d) {
-		this.id=d.id;
-		this.nombre=d.nombre;
-		this.creador=d.creador;
-		this.fecha_produccion=d.fecha_produccion;
-		this.canciones=d.canciones;		
+		this.id = d.id;
+		this.nombre = d.nombre;
+		this.creador = d.creador;
+		this.fecha_produccion = d.fecha_produccion;
+		this.canciones = d.canciones;
 	}
-	
+
 	public void detatch() {
-		persist=false;
+		persist = false;
 	}
-	
+
 	public boolean ispersist() {
 		return persist;
 	}
-	
+
 	public void setPersist(boolean persist) {
-		this.persist=persist;
+		this.persist = persist;
 	}
-	
+
 	public void setId(int id) {
 		super.setId(id);
-		if(persist) {
+		if (persist) {
 			save();
 		}
 	}
-	
+
 	public void setNombre(String nombre) {
 		super.setNombre(nombre);
-		if(persist) {
+		if (persist) {
 			save();
 		}
 	}
-	
+
 	public void setFoto(String foto) {
 		super.setFoto(foto);
-		if(persist) {
+		if (persist) {
 			save();
 		}
 	}
-	
-	//Relación de miguel 
+
+	// Relación de miguel
 	public void setCreador(Artista creador) {
-		
+
 	}
-	
+
 	public void setFecha_produccion(Date fecha_produccion) {
 		super.setFecha_produccion(fecha_produccion);
-		if(persist) {
+		if (persist) {
 			save();
 		}
 	}
-	
+
 	public void setCanciones(List<Cancion> canciones) {
 		super.setCanciones(canciones);
-		if(persist) {
+		if (persist) {
 			save();
 		}
 	}
-	
+
 	/**
 	 * Metodo que guarda una cancion en la base de datos
 	 *
@@ -131,23 +133,134 @@ public class DiscoDAO extends Disco {
 
 		return result;
 	}
-	
-	
-	
+
+	/**
+	 * Metodo que lista todas los discos de la base de datos
+	 * 
+	 * @return Devuelve una lista de discos
+	 */
+	public static List<Disco> selectAll() {
+		return selectAll("");
+	}
+
+	/**
+	 * Funcion que selecciona por nombre el disco de la base de datos
+	 * 
+	 * @param pattern
+	 * @return Devuelve una lista de Discos
+	 */
+	public static List<Disco> selectAll(String pattern) {
+		List<Disco> result = new ArrayList<Disco>();
+
+		try {
+			manager = Connection.connectToMysql();
+			manager.getTransaction().begin();
+
+			String q = "FROM disco";
+
+			if (pattern.length() > 0) {
+				q += "WHERE nombre LIKE ?";
+			}
+
+			if (pattern.length() > 0) {
+				result = manager.createQuery("FROM disco WHERE nombre LIKE '" + pattern + "%'").getResultList();
+
+			} else {
+				result = manager.createQuery("FROM disco").getResultList();
+			}
+
+			manager.getTransaction().commit();
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		return result;
+	}
+
+	/**
+	 * ´ Función que selecciona por id el disco de la base de datos
+	 * 
+	 * @param id , id por lo que se filtra el select
+	 * @return devuelve un disco
+	 */
+	public static Disco selectAllForID(int id) {
+		Disco result = null;
+		try {
+			manager = Connection.connectToMysql();
+			manager.getTransaction().begin();
+
+			String q = "FROM disco WHERE idb=?";
+
+			List<Disco> Discos = manager.createQuery(q + id).getResultList();
+
+			if (Discos.size() != 0) {
+				result = Discos.get(0);
+			}
+			manager.getTransaction().commit();
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		}
+
+		return result;
+	}
+
+	/**
+	 * Funcion que selecciona por nombre el primer disco de la base de datos que sea
+	 * por el nombre
+	 * 
+	 * @param nombre Palabra por lo que filtra el select
+	 * @return devuelve un disco
+	 */
+	public static Disco selectAllforNombre(String nombre) {
+		Disco result = null;
+
+		try {
+			manager = Connection.connectToMysql();
+			manager.getTransaction().begin();
+
+			String q = "FROM disco WHERE nombre =";
+
+			List<Disco> discos = manager.createQuery("FROM disco WHERE nombre = '" + nombre + "'").getResultList();
+
+			if (discos.size() != 0) {
+				result = discos.get(0);
+			}
+			
+			manager.getTransaction().commit();
+		} catch (Exceptio ex) {
+			System.out.println(ex);
+
+		}
+		return result;
+	}
+
+	/**
+	 * Borra de la base de datos un Disco
+	 *
+	 * @return -1 si no se ha borrado el disco o el id del disco eliminado
+	 */
+	public boolean remove() {
+		boolean result = false;
+
+		if (this.id > 0) {
+
+			try {
+				manager = Connection.connectToMysql();
+				manager.getTransaction().begin();
+
+				if (manager.createQuery("DELETE FROM disco WHERE ID =" + this.id).executeUpdate() == 1) {
+					result = true;
+				}
+
+				manager.getTransaction().commit();
+
+			} catch (Exception ex) {
+				System.out.println(ex);
+			}
+
+		}
+		return result;
+	}
+
 }
-	
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
